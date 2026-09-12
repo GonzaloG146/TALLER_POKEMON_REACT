bef0e0520b43165cda589a386756ba8d83eb0d06
GonzaloG146 <gg260877@gmail.com>
2026-09-12 17:57:30 +0000

    Creacion del proyectoy conexto Global

---
+++ Buscador-Pokemon-React/src/context/PokemonContext.tsx
@@ -0,0 +1,74 @@
+import React, { createContext, useContext, useState, useEffect, useLayoutEffect } from 'react';
+
+export interface Usuario {
+    id: number;
+    nombreCompleto: string;
+    documentos: { tipo: string, numero:string};
+    fechaNacimiento: string;
+    correo: string;
+    datosPersonales: boolean;
+    fechaRegistro: string,
+
+
+
+}
+
+export interface PokemonTarjeta {
+    id: number;
+    name: string;
+    image: string;
+    type: string;
+    baseExperoemce: string;
+    esFavorito?: boolean;
+
+}
+
+interface PokemonContextType {
+    entrenadores : Usuario[];
+    entrandorActivo : Usuario | null;
+    mochilaActual : PokemonTarjeta[];
+    seleccionarEntrenador : (usuario: Usuario) => void;
+    registrarEntrenador : (usuario: Usuario) => void;
+    guardarMochila : (pokemon : PokemonTarjeta) => void;
+    actualizarFavorito : (pokemonID: number) => void;
+    eliminarPokemon : (pokemonID: number) => void;
+
+
+}
+
+const PokemonContext = createContext<PokemonContextType | undefined> (undefined);
+
+export const PokemonProvider : React.FC<{ children : React.ReactNode}> = ({ children }) => {
+    const [entrandores,setEntrenadores] = useState<Usuario[]>([]);
+    const [entrenadorActivo,setEntrenadorActivo] = useState<Usuario[] | null> (null);
+    const [mochilaActual,setMochilaActual] = useState<PokemonTarjeta[] | null> (null);
+
+    useLayoutEffect(() => {
+        const data = localStorage.getItem('lista_entrenadores');
+        if(data){
+            const lista : Usuario[]= JSON.parse(data); 
+            setEntrenadores(lista);
+
+            const idActivo = localStorage.getItem('entrenador_Activo_id');
+
+            if(idActivo) {
+                const encontrado = lista.find(u => u.id.toString() === idActivo);
+                if (encontrado) seleccionarEntrenador(encontrado);
+            }
+        }
+    },[]);
+
+    const cargarMochilaEntrenador = (usuarioId: number) => {
+        const data = localStorage.getItem(`mochila_${usuarioId}`);
+        setMochilaActual(data ? JSON.parse(data) : []);
+
+    }
+
+    const seleccionarEntrenador = (usuario : Usuario) => {
+        setEntrenadorActivo(usuario);
+        
+        localStorage.setItem('entrenador_activo_id', usuario.id.toString());
+        cargarMochilaEntrenador(usuario.id);
+    }
+
+} 
